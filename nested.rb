db20c8575974a312055c0441d8cac2833dc47aef
Anya Brun <tsbrun@icloud.com>
2020-07-30 15:17:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,6 +82,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+				:yukihiro_matsumoto => {
+				#   :known_for => "Ruby",
+				#    :languages => ["LISP", "C"]
+				# }
      }
 
 
