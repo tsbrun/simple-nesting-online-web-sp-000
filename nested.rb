254c3b1247831781dd2ca91ee1f505bfa82f2dbc
Anya Brun <tsbrun@icloud.com>
2020-07-30 15:17:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,13 +62,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
- :yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }
-
-	programmer_hash =
+  programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
